8f357dbdef7be621af6580a14708f996bfcfe8af
PrajaktaShirkeSource <prajakta.dawange@gmail.com>
2026-04-20 09:50:22 -0400

    Recursion Issue

--- IMS.CoreBusiness/ProductInventory.cs
+++ IMS.CoreBusiness/ProductInventory.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace IMS.CoreBusiness
 {
     public class ProductInventory
     {
+        [ForeignKey("ProductId")]
         public int ProductId { get; set; }
-        public Product? Product { get; set; }
+        [JsonIgnore]
+        public virtual Product Product { get; set; }
+
+        [ForeignKey("InventoryId")]
         public int InventoryId { get; set; }
-        public Inventory? Inventory { get; set; }
+        [JsonIgnore]
+        public virtual Inventory Inventory { get; set; }
+
+        //public int ProductId { get; set; }
+        
+        //[JsonIgnore]
+        //public Product? Product { get; set; }
+        //public int InventoryId { get; set; }
+
+        //[JsonIgnore]
+        //public Inventory? Inventory { get; set; }
         public int InventoryQuantity { get; set; }
+
+
     }
 }
